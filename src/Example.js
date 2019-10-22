935ff29680aba3cf41c0b81de8d45aa6a830d7cd
Ayesha W <ayesha.wan@youdo.co.nz>
2019-10-22 15:38:40 +1300

    Init commit

---
+++ src/Example.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+
+const Example = () => {
+  const [count, setCount] = useState(0)
+}
+//declare a new state var, we'll call 'count'
+
+export default Example
